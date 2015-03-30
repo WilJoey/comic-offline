52b10202b09e170f3b93713273cf089dd6a96036
WilJoey Lin <wiljoey@gmail.com>
2015-03-30 14:58:27 +0800

    add ntvs to dev.

---
+++ libs/parseHtml.js
@@ -0,0 +1,16 @@
+﻿var cheerio = require('cheerio');
+
+module.exports = {
+    parseCollection: function(html) {
+        var $ = cheerio.load(html, {
+            normalizeWhitespace: false,
+            xmlMode: false,
+            decodeEntities: false //dont decode
+        });
+        //var info = $('#info').find('td').text().trim().replace(/(\r\n|\n|\r|\s)/g,'');
+        var info = $('#info');//.find('td').html().trim(); //.replace(/(\r\n|\n|\r|\s)/g,'');
+        return info.length;
+        
+    }
+
+};
---
+++ test/UnitTest1.js
@@ -0,0 +1,29 @@
+﻿var assert = require('assert');
+var fs = require('fs');
+var parser = require('../libs/parseHtml.js');
+
+describe('Test Suite 1', function() {
+    //it('Test 1', function() {
+    //    assert.ok(true, "This shouldn't fail");
+    //});
+
+    //it('Test 2', function() {
+    //    assert.ok(1 === 1, "This shouldn't fail");
+    //    //assert.ok(false, "This should fail");
+    //    assert.equal(false, false, "This should fail");
+    //});
+
+    it('Test Parse Html Collection', function () {
+        
+        fs.readFile('../data/book-collection.txt', function (err, data) {
+            if (err) {
+                throw err;
+            }
+            console.log(data);
+            var result = parser.parseCollection(data);
+            var actual = 12;
+            assert.equal(result, actual, "This should equal.");
+        });
+        
+    });
+});
